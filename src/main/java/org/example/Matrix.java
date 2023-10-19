e3540ae777ef65bd018a386932b1986c26960524
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-10-19 23:51:57 +0200

    first

---
+++ src/main/java/org/example/Matrix.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Matrix extends Remote {
+    int[][] multiply(int[][] matrixA, int[][] matrixB) throws RemoteException;
+}
+
